07e83103a7d5fb5407271fe990dc8f0ea580784f
tomek <tomek.ogorek@netblink.net>
2019-06-10 11:20:26 +0200

    changes for d2.2

--- inplaceeditform/urls.py
+++ inplaceeditform/urls.py
@@ -14,11 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this programe.  If not, see <http://www.gnu.org/licenses/>.
 
-try:
-    from django.urls import patterns, url
-    # from django.conf.urls import url
-except ImportError:  # Django < 1.4
-    from django.conf.urls.defaults import patterns, url
+from django.urls import url
 
 
 # urlpatterns = patterns('inplaceeditform.views',
